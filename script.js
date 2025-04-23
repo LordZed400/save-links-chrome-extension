030846138fb498fc1435c9f74a2d142e948a7a46
Swagat Prasad Niroula <swagat.niroula@tum.de>
2025-04-23 10:26:23 +0200

    feat: group links into categories

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function setLinks() {
   var savedLinksContainer = document.getElementById("linksContainer");
+  var categoryTemplate = document.getElementById("categoryTemplate");
   var linkTemplate = document.getElementById("linkTemplate");
   var noLinksMessage = "You haven't saved any links yet!!!";
 
@@ -14,35 +15,64 @@ function setLinks() {
       noLinksElement.textContent = noLinksMessage;
       savedLinksContainer.appendChild(noLinksElement);
     } else {
+      const grouped = {};
       links.forEach((link) => {
-        var listItem = linkTemplate.content.cloneNode(true);
+        if (!grouped[link.category]) {
+          grouped[link.category] = [];
+        }
+        grouped[link.category].push(link);
+      });
 
-        var titleElement = listItem.querySelector(".link-title");
-        var linkElement = listItem.querySelector(".link-url");
-        var editButton = listItem.querySelector(".edit-button button");
-        var copyButton = listItem.querySelector(".copy-button button");
-        var deleteButton = listItem.querySelector(".delete-button button");
+      const sortedCategories = Object.keys(grouped).sort();
 
-        titleElement.textContent = link.category + " | " + link.title;
-        linkElement.textContent = link.url;
-        linkElement.onclick = () => window.open(link.url, "_blank");
-        copyButton.onclick = () => {
-          navigator.clipboard.writeText(link.url).then(() => {
-            alert("Link copied to clipboard!");
-          });
+      sortedCategories.forEach((category) => {
+        var categoryItem = categoryTemplate.content.cloneNode(true);
+        
+        var categoryWrapper = categoryItem.querySelector(".category-container");
+        var categoryTitle = categoryItem.querySelector(".category-title");
+        var collapseHeader = categoryItem.querySelector(".category-header");
+
+        categoryTitle.textContent = category;
+        
+        collapseHeader.onclick = () => {
+          linksList.classList.toggle("collapsed");
         };
-        deleteButton.onclick = () => {
-          var updatedLinks = links.filter(
-            (savedLink) => savedLink.id !== link.id
-          );
-          chrome.storage.sync
-            .set({ savedLinksForLater: updatedLinks })
-            .then(() => {
-              setLinks();
+        
+        const linksList = document.createElement("div");
+        linksList.className = "links-list";
+        grouped[category].forEach((link) => {
+          var listItem = linkTemplate.content.cloneNode(true);
+
+          var titleElement = listItem.querySelector(".link-title");
+          var linkElement = listItem.querySelector(".link-url");
+          var editButton = listItem.querySelector(".edit-button");
+          var copyButton = listItem.querySelector(".copy-button");
+          var deleteButton = listItem.querySelector(".delete-button");
+
+          titleElement.textContent = link.title;
+          linkElement.textContent = link.url;
+          linkElement.onclick = () => window.open(link.url, "_blank");
+          copyButton.onclick = () => {
+            navigator.clipboard.writeText(link.url).then(() => {
+              alert("Link copied to clipboard!");
             });
-        };
+          };
+          deleteButton.onclick = () => {
+            var updatedLinks = links.filter(
+              (savedLink) => savedLink.id !== link.id
+            );
+            chrome.storage.sync
+              .set({ savedLinksForLater: updatedLinks })
+              .then(() => {
+                setLinks();
+              });
+          };
 
-        savedLinksContainer.appendChild(listItem);
+          linksList.appendChild(listItem);
+        });
+
+        categoryWrapper.appendChild(linksList);
+        savedLinksContainer.appendChild(categoryWrapper);
       });
     }
   });
@@ -65,7 +95,7 @@ function saveLink() {
 }
 
 function getCurrentTabInfo(callback) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     if (tabs.length === 0) return;
     const tab = tabs[0];
     const title = tab.title;
